bfe14ef758659928e56f3bfb9db6b5c9ddf5622a
jenildesai25 <jenil.desai25@gmail.com>
2018-10-26 00:03:48 -0500

    commented and completed Knn.py

--- ATNT50/Knn.py
+++ ATNT50/Knn.py
@@ -9,63 +9,124 @@ class Knn:
         self.k = k
 
     def load_train_test_data(self, train_data, test_data):
+        """
+
+        :param train_data: train data(raw data) is file we wanted our algorithm to train with so we can use that result with test data.
+        :param test_data: test data(raw data) for checking that our prediction is right or not and finding the accuracy.
+        :return: well formed train and test data with having rows as one image and index is label of the image.
+        """
         try:
+            # next line will give you transposed and well formatted train data.
             train_data = ReadData.load_data(train_data)
+            # next line will give you transposed and well formatted test data.
             test_data = ReadData.load_test_data(test_data)
             return train_data, test_data
         except Exception as e:
             print(e)
 
     def calculate_distance(self, train_data, test_data):
-        d = {}
+        """
+
+        :param train_data: formatted train data that we get from load_train_test_data function.
+        :param test_data: formatted test data that we get from load_train_test_data function.
+        :return: dictionary with having key as test data index i.e in our case ['1','4','5','3','2']. be careful keys are in string.
+        and values are sorted euclidean distance with label from train data.
+        container = {'1':[(1111,1),(131241,3),...]}
+        '1' is the key from test data and (1111) is the distance between 1st instance of train data with 1st data instance from test data.
+        1 is the label of train data.
+        """
+        # container is the dictionary that stores all the test label as key and values as 0th index having distances and 1st index as having label of train data.
+        container = {}
+        # iterate over index so we can store index of test data and store it in container as key.
         count = 0
-        euclidean_distance = []
         try:
+            # i is the test data 1st value. As i increases we are getting immediate next rows as i.
             for i in test_data.values:
-                d[test_data.index[count]] = []
+                # container[test_data.index[count]] makes the key as container = {'1':[],'2':[],...}. Here '1' is the index of test data.
+                container[test_data.index[count]] = []
+                # index is the variable that holds value for finding label from train data.i.e train_data.index[index] will give me the label name of particular row.
+                index = 0
+                # j is the train data 1st value. As j increases we are getting immediate next rows as j.
                 for j in train_data.values:
+                    # temporary variable to store distance between rows between train data and test data.
                     temp = distance.euclidean(j, i)
-                    euclidean_distance.append((temp, test_data.index[count]))
-                    d[test_data.index[count]].append(temp)
+                    # container[test_data.index[count]].append((temp, train_data.index[index])) append distance, and train data index of that distance
+                    container[test_data.index[count]].append((temp, train_data.index[index]))
+                    # index value increases as per row changes for train data.
+                    index += 1
+                # count increase as train data row finishes and we need another row from test data.
                 count += 1
-            for k, v in d.items():
+            # this three lines sorting the dictionary and giving sorted distance as first.
+            for k, v in container.items():
                 train_data[k] = v
-                d[k] = sorted(d[k])
-            sorted_euclidean_distance = sorted(euclidean_distance, key=lambda x: x[0])
-            return [distance_data[1] for distance_data in sorted_euclidean_distance[:self.k]]
-            # return sorted_euclidean_distance
-            # return d
+                container[k] = sorted(container[k])
+            return container
         except Exception as e:
             print(e)
 
     def sort_data_frame(self, sorted_euclidean_distance):
+        """
+
+        :param sorted_euclidean_distance: It is a dictionary that has key as test data label(index is the one that is giving me label as per my case).
+        values are sorted ascending. i.e {'1':[(111,1),(123,2),...]} here '1' is the label from test data and values are sorted distance and train data label as (111,1).
+        :return: returns k label as key is test data labels and values are k nearest label. i.e {'1':[1,1,1],'2':[2,2,2],...}. here '1' is the test data index(label)
+        and [1,1,1] are nearest 3 label values from test data.
+        """
         try:
-            # data_frame_1 = data.iloc[:, -2:-1]
-            # data_frame_1 = data_frame_1.sort_values(by=['2'], ascending=True)
-            # data_frame_2 = data.iloc[:, -3:-2]
-            # data_frame_2 = data_frame_2.sort_values(by=['5'], ascending=True)
-            # data_frame_3 = data.iloc[:, -4:-3]
-            # data_frame_3 = data_frame_3.sort_values(by=['4'], ascending=True)
-            # data_frame_4 = data.iloc[:, -5:-4]
-            # data_frame_4 = data_frame_4.sort_values(by=['1'], ascending=True)
-            # data_frame_5 = data.iloc[:, -1]
-            # data_frame_5 = data_frame_5.sort_values(by=['3'], ascending=True)
-            k_nearest_neighbour = sorted_euclidean_distance[:self.k]
-            k_nearest_neighbour_label = []
-            for i in k_nearest_neighbour:
-                k_nearest_neighbour_label.append(i[1])
-            return k_nearest_neighbour_label
+            # dict_of_labels is the dictionary that stores k nearest label as values and test data label as key.
+            dict_of_labels = {}
+            # iterate over dict_of_labels and store values in it.
+            for k, v in sorted_euclidean_distance.items():
+                # dict_of_labels[k] is the first index of test data label.
+                dict_of_labels[k] = []
+                # iterate k times over data and store the label as value.
+                for i in range(self.k):
+                    # dict_of_labels[k].append(v[:self.k][i][1]) gives you the label from train data.
+                    dict_of_labels[k].append(v[:self.k][i][1])
+            return dict_of_labels
         except Exception as e:
             print(e)
 
-    def find_accuracy(self, test_data, nearest_label):
-        # most_occurrence = max(nearest_label, key=nearest_label.count)
-        label_value_of_test_data = list(test_data.index)
-        correct_label_choice = 0
-        for i in label_value_of_test_data:
-            if i in nearest_label:
-                correct_label_choice += 1
-        return (correct_label_choice / float(len(label_value_of_test_data))) * 100.0
+    def count_matching_label(self, nearest_label):
+        """
+
+        :param nearest_label: takes dictionary with having information of nearest label as value and train data index as key.i.e {'1':[1,1,1],'2':[2,2,2],...}
+        :return: return highest occurrence of test data index.
+        """
+        # store value of train data label.
+        count = []
+        for k, v in nearest_label.items():
+            # count.append(max(v, key=v.count)) append highest occurrence of value of key.
+            count.append(max(v, key=v.count))
+        return count
+
+    def find_accuracy(self, nearest_neighbour_label, occurrence_of_test_data):
+        """
+
+        :param nearest_neighbour_label: takes dictionary with having information of nearest label as value and train data index as key.i.e {'1':[1,1,1],'2':[2,2,2],...}
+        :param occurrence_of_test_data: takes list of predicted label values for test data.
+        :return: print all the required information.
+        """
+        print('k is: {}\n'.format(self.k))
+        print('KNN: \n')
+        count = 0
+        try:
+            # iterate over key and print all the information.
+            for k, v in nearest_neighbour_label.items():
+                print('Test Label:{} \nNeighbors Label:{} \nClassification: {}\n'.format(k, v, occurrence_of_test_data[count]))
+                count += 1
+            accuracy_percentage = 0
+            count = 0
+            # iterate over key and count prediction.
+            for k, v in nearest_neighbour_label.items():
+                if int(k) == occurrence_of_test_data[count]:
+                    accuracy_percentage = accuracy_percentage + (100 // len(nearest_neighbour_label))
+                    count += 1
+                else:
+                    count += 1
+            print('Accuracy of test data is: {}%'.format(accuracy_percentage))
+        except Exception as e:
+            print(e)
 
 
 if __name__ == '__main__':
@@ -73,7 +134,9 @@ if __name__ == '__main__':
     train_data_sample, test_data_sample = knn_object.load_train_test_data("C:/Users/jenil/OneDrive - University of Texas at Arlington/UTA/sem 3/CSE 5334/Project1/ATNT50/trainDataXY.txt", "C:/Users/jenil/OneDrive - University of Texas at Arlington/UTA/sem 3/CSE 5334/Project1/ATNT50/testDataXY.txt")
     data_with_euclidean_distance = knn_object.calculate_distance(train_data_sample, test_data_sample)
     nearest_neighbour = knn_object.sort_data_frame(data_with_euclidean_distance)
-    print('Accuracy is {}'.format(knn_object.find_accuracy(test_data_sample, nearest_neighbour)))
+    occurrence_label = knn_object.count_matching_label(nearest_neighbour)
+    knn_object.find_accuracy(nearest_neighbour, occurrence_label)
+    # print('Accuracy is {}'.format(knn_object.find_accuracy(nearest_neighbour)))
     # neigh = KNeighborsClassifier(n_neighbors=3)
-    # neigh.fit(train_data_sample, train_data_sample.columns)
-    # print(neigh.score(test_data_sample, test_data_sample.columns))
+    # neigh.fit(train_data_sample, train_data_sample.index)
+    # print(neigh.score(test_data_sample, test_data_sample.index))

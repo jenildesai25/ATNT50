46d623b250223001ba40dd659e20580b74231559
Jinesh Modi <jinesh1994@gmail.com>
2018-10-30 16:24:16 -0500

    Task C and D completed also optimized it

--- ATNT50/Task_D.py
+++ ATNT50/Task_D.py
@@ -4,14 +4,13 @@ from matplotlib import pyplot
 
 if __name__ == '__main__':
     prediction = []
-    classifier_object = Centroid.Centroid()
     # input = input('Alphabets: ')
     input = "hijklmnopq"
     class_ids = Task_E.letter_2_digit_convert(input)
     # print(class_ids)
     splits = [(5, 34), (10, 29), (15, 24), (20, 19), (25, 24), (30, 9), (35, 4)]
+    data = Task_E.pickDataClass("../HandWrittenLetters.txt", class_ids)
     for train, test in splits:
-        data = Task_E.pickDataClass("../HandWrittenLetters.txt", class_ids)
         # print(data)
         trainX, trainY, testX, testY, train_data_with_labels, test_data_with_labels = Task_E.splitData2TestTrain(data, 39, test)
         # print(trainX)
@@ -19,6 +18,7 @@ if __name__ == '__main__':
         # trainX = Task_E.to_data_frame(trainX, trainY)
         # testX = Task_E.to_data_frame(testX, testY)
         # print(trainX)
+        classifier_object = Centroid.Centroid()
         classifier_object.train_data_and_find_mean(train_data_with_labels)
         classified_data = classifier_object.classify(test_data_with_labels.values)
         predict = classifier_object.score(classified_data)
